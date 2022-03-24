3efa2406b5294577043c5c5f07c981128685775f
Piyushrai558 <piyushrai558@gmail.com>
2022-03-24 11:54:58 +0530

    first commit

---
+++ src/main/java/com/example/spring/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.example.spring;
+import org.springframework.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Deploying rest api";
+	}
+
+}
